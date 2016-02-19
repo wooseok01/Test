fb79d7a5ba263d1b6e790b0123f0d9cf3de0c3d9
SongWooseok <wooseok01@naver.com>
2016-02-19 14:19:28 +0900

    no message

--- FileDetectingProject/FileDetectingExpress/routes/index.js
+++ FileDetectingProject/FileDetectingExpress/routes/index.js
@@ -135,6 +135,7 @@ router.post('/getDirData', function(req, res, next){
 router.post('/upload', function(req, res, next){
     var post = req.body;
     console.log('upload file name ---->> ' + req.files);
+    console.log('post --> ' + post);
 });
 
 router.get(dir+'/*', function(req,res,next){
